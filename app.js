24df18713e32de11bf8868aee2ccaaeceb482137
Peter Ayad <swordoflight8847@yahoo.com>
2021-07-12 13:20:54 -0700

    updated all files project-9

--- app.js
+++ app.js
@@ -1,174 +1,203 @@
-(function () {
-    var requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame || window.msRequestAnimationFrame || function (callback) {
-            window.setTimeout(callback, 1000 / 60);
-        };
-    window.requestAnimationFrame = requestAnimationFrame;
-})();
+// baguetteBox.run('.list');
 
-// Terrain stuff.
-var background = document.getElementById("bgCanvas"),
-    bgCtx = background.getContext("2d"),
-    width = window.innerWidth,
-    height = document.body.offsetHeight;
+// // Code Snippet from //
+// // https://www.geeksforgeeks.org/search-bar-using-html-css-and-javascript/ //
+// function search_photo() {
+//     let input = document.getElementById('searchbar').value;
+//     input=input.toLowerCase();
+//     let photo = document.getElementsByClassName('photos');
 
-(height < 400) ? height = 400 : height;
+//     for(i = 0; i < photo.length; i++) {
+//         if (!photo[i].innerHTML.toLowerCase().includes(input)) {
+//             photo[i].style.display='none';
+//         } else {
+//             photo[i].style.display='list-item';
+//         }
+//     }
+// }
 
-background.width = width;
-background.height = height;
 
-function Terrain(options) {
-    options = options || {};
-    this.terrain = document.createElement("canvas");
-    this.terCtx = this.terrain.getContext("2d");
-    this.scrollDelay = options.scrollDelay || 90;
-    this.lastScroll = new Date().getTime();
 
-    this.terrain.width = width;
-    this.terrain.height = height;
-    this.fillStyle = options.fillStyle || "#191D4C";
-    this.mHeight = options.mHeight || height;
+const alertBanner = document.getElementById("alert");
+const modalWindow = document.getElementById("modal-window");
+const modalWindow2 = document.getElementById("modal-window2");
+const modalWindow3 = document.getElementById("modal-window3");
+const modalWindow4 = document.getElementById("modal-window4");
+const modalWindow5 = document.getElementById("modal-window5");
+const modalWindow6 = document.getElementById("modal-window6");
+const modalWindow7 = document.getElementById("modal-window7");
+const modalWindow8 = document.getElementById("modal-window8");
+// const searchInput = document.getElementById("search-input");
+// const filter = input.value.toUpperCase();
+// const ul = document.getElementById("search-list");
+// const li = ul.getElementsByTagName("li");
+// const modalImage1 = document.getElementById("modal-image1");
+const mercury = document.getElementById("mercury");
+const venus = document.getElementById("venus");
+const earth = document.getElementById("earth");
+const mars = document.getElementById("mars");
+const jupiter = document.getElementById("jupiter");
+const saturn = document.getElementById("saturn");
+const uranus = document.getElementById("uranus");
+const neptune = document.getElementById("neptune");
+const span = document.getElementById("close");
+const span2 = document.getElementById("close2");
+const span3 = document.getElementById("close3");
+const span4 = document.getElementById("close4");
+const span5 = document.getElementById("close5");
+const span6 = document.getElementById("close6");
+const span7 = document.getElementById("close7");
+const span8 = document.getElementById("close8");
 
-    // generate
-    this.points = [];
-
-    var displacement = options.displacement || 140,
-        power = Math.pow(2, Math.ceil(Math.log(width) / (Math.log(2))));
-
-    // set the start height and end height for the terrain
-    this.points[0] = this.mHeight;//(this.mHeight - (Math.random() * this.mHeight / 2)) - displacement;
-    this.points[power] = this.points[0];
-
-    // create the rest of the points
-    for (var i = 1; i < power; i *= 2) {
-        for (var j = (power / i) / 2; j < power; j += power / i) {
-            this.points[j] = ((this.points[j - (power / i) / 2] + this.points[j + (power / i) / 2]) / 2) + Math.floor(Math.random() * -displacement + displacement);
-        }
-        displacement *= 0.6;
+alertBanner.addEventListener("click", (e) => {
+    const element = e.target;
+    if (element.classList.contains("alert-banner-close")) {
+        alertBanner.style.display= "none"
     }
+});
 
-    document.body.appendChild(this.terrain);
+
+
+modalWindow.style.display = "none";
+
+mercury.addEventListener("click", () => {
+    modalWindow.style.display = "block"
+});
+
+span.addEventListener("click", (e) => {
+    const element = e.target;
+    if (element.classList.contains("close")) {
+        modalWindow.style.display = "none"
+    }
+});
+
+modalWindow2.style.display = "none";
+
+venus.addEventListener("click", () => {
+    modalWindow2.style.display = "block"
+});
+
+
+span2.addEventListener("click", (e) => {
+    const element = e.target;
+    if (element.classList.contains("close2")) {
+        modalWindow2.style.display = "none"
+    }
+});
+
+modalWindow3.style.display = "none";
+
+earth.addEventListener("click", () => {
+    modalWindow3.style.display = "block"
+});
+
+span3.addEventListener("click", (e) => {
+    const element = e.target;
+    if (element.classList.contains("close3")) {
+        modalWindow3.style.display = "none"
+    }
+});
+
+modalWindow4.style.display = "none";
+
+mars.addEventListener("click", () => {
+     modalWindow4.style.display = "block"
+});
+
+span4.addEventListener("click", (e) => {
+    const element = e.target;
+    if (element.classList.contains("close4")) {
+        modalWindow4.style.display = "none"
+    }
+});
+
+modalWindow5.style.display = "none";
+
+jupiter.addEventListener("click", () => {
+    modalWindow5.style.display = "block"
+});
+
+span5.addEventListener("click", (e) => {
+    const element = e.target;
+    if (element.classList.contains("close5")) {
+        modalWindow5.style.display = "none"
+    }
+});
+
+modalWindow6.style.display = "none";
+
+saturn.addEventListener("click", () => {
+    modalWindow6.style.display = "block"
+});
+
+span6.addEventListener("click", (e) => {
+    const element = e.target;
+    if (element.classList.contains("close6")) {
+        modalWindow6.style.display = "none"
+    }
+});
+
+modalWindow7.style.display = "none";
+
+uranus.addEventListener("click", () => {
+    modalWindow7.style.display = "block"
+});
+
+span7.addEventListener("click", (e) => {
+    const element = e.target;
+    if (element.classList.contains("close7")) {
+        modalWindow7.style.display = "none"
+    }
+});
+
+modalWindow8.style.display = "none";
+
+neptune.addEventListener("click", () => {
+    modalWindow8.style.display = "block"
+});
+
+span8.addEventListener("click", (e) => {
+    const element = e.target;
+    if (element.classList.contains("close8")) {
+        modalWindow8.style.display = "none"
+    }
+});
+
+function searchFilter() {
+
+var searchInput, filter, ul, li, a, i, txtValue;
+searchInput = document.getElementById("search-input");
+filter = searchInput.value.toUpperCase();
+ul = document.getElementById("search-list");
+li = ul.getElementsByTagName("li");
+
+
+for (i = 0; i < li.length; i++) {
+    a = li[i].getElementsByTagName("a")[0];
+    txtValue = a.textContent || a.innerText;
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        li[i].style.display = "";
+    } else {
+        li[i].style.display = "none";
+    }
+}
 }
 
 
-
-Terrain.prototype.update = function () {
-    // draw the terrain
-    // this.terCtx.clearRect(0, 0, width, height);
-    // this.terCtx.fillStyle = this.fillStyle;
     
-    // if (new Date().getTime() > this.lastScroll + this.scrollDelay) {
-    //     this.lastScroll = new Date().getTime();
-    //     this.points.push(this.points.shift());
+
+    // modalImage1.onclick = function() {
+    //     modalWindow.style.display = "block";
     // }
 
-    // this.terCtx.beginPath();
-    // for (var i = 0; i <= width; i++) {
-    //     if (i === 0) {
-    //         this.terCtx.moveTo(0, this.points[0]);
-    //     } else if (this.points[i] !== undefined) {
-    //         this.terCtx.lineTo(i, this.points[i]);
-    //     }
-    // }
 
-    // this.terCtx.lineTo(width, this.terrain.height);
-    // this.terCtx.lineTo(0, this.terrain.height);
-    // this.terCtx.lineTo(0, this.points[0]);
-    // this.terCtx.fill();
-}
+// function openModal1() {
+//     modalWindow.style.display = "block";
+// }
 
+// modalWindow.addEventListener("click", () => {
+//     modalWindow.style.display= "none";
+//     if () {
 
-// Second canvas used for the stars
-bgCtx.fillStyle = '#05004c';
-bgCtx.fillRect(0, 0, width, height);
-
-// stars
-function Star(options) {
-    this.size = Math.random() * 2;
-    this.speed = Math.random() * .05;
-    this.x = options.x;
-    this.y = options.y;
-}
-
-Star.prototype.reset = function () {
-    this.size = Math.random() * 2;
-    this.speed = Math.random() * .05;
-    this.x = width;
-    this.y = Math.random() * height;
-}
-
-Star.prototype.update = function () {
-    this.x -= this.speed;
-    if (this.x < 0) {
-        this.reset();
-    } else {
-        bgCtx.fillRect(this.x, this.y, this.size, this.size);
-    }
-}
-
-function ShootingStar() {
-    this.reset();
-}
-
-ShootingStar.prototype.reset = function () {
-    this.x = Math.random() * width;
-    this.y = 0;
-    this.len = (Math.random() * 80) + 10;
-    this.speed = (Math.random() * 10) + 6;
-    this.size = (Math.random() * 1) + 0.1;
-    // this is used so the shooting stars arent constant
-    this.waitTime = new Date().getTime() + (Math.random() * 3000) + 500;
-    this.active = false;
-}
-
-ShootingStar.prototype.update = function () {
-    if (this.active) {
-        this.x -= this.speed;
-        this.y += this.speed;
-        if (this.x < 0 || this.y >= height) {
-            this.reset();
-        } else {
-            bgCtx.lineWidth = this.size;
-            bgCtx.beginPath();
-            bgCtx.moveTo(this.x, this.y);
-            bgCtx.lineTo(this.x + this.len, this.y - this.len);
-            bgCtx.stroke();
-        }
-    } else {
-        if (this.waitTime < new Date().getTime()) {
-            this.active = true;
-        }
-    }
-}
-
-var entities = [];
-
-// init the stars
-for (var i = 0; i < height; i++) {
-    entities.push(new Star({
-        x: Math.random() * width,
-        y: Math.random() * height
-    }));
-}
-
-// Add 2 shooting stars that just cycle.
-entities.push(new ShootingStar());
-entities.push(new ShootingStar());
-entities.push(new Terrain({mHeight : (height/2)-120}));
-entities.push(new Terrain({displacement : 120, scrollDelay : 50, fillStyle : "rgb(17,20,40)", mHeight : (height/2)-60}));
-entities.push(new Terrain({displacement : 100, scrollDelay : 20, fillStyle : "rgb(10,10,5)", mHeight : height/2}));
-
-//animate background
-function animate() {
-    bgCtx.fillStyle = '#110E19';
-    bgCtx.fillRect(0, 0, width, height);
-    bgCtx.fillStyle = '#ffffff';
-    bgCtx.strokeStyle = '#ffffff';
-
-    var entLen = entities.length;
-
-    while (entLen--) {
-        entities[entLen].update();
-    }
-    requestAnimationFrame(animate);
-}
-animate();
+//     }
+// });
